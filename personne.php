b5c8305bfd43e96c15e8a89ab3162e79d7e99cb4
Rregis <robin.regis78@sfr.fr>
2016-11-02 10:48:37 +0100

    add class personne

---
+++ personne.php
@@ -0,0 +1,64 @@
+<?
+/**
+* 
+*/
+class Personne
+{
+	
+	private $name;
+	private $prenom;
+
+	function __construct($name, $prenom)
+	{
+		$this->name = $name;
+		$this->prenom = $prenom;
+	}
+
+    /**
+     * Gets the value of name.
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Sets the value of name.
+     *
+     * @param mixed $name the name
+     *
+     * @return self
+     */
+    private function _setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of prenom.
+     *
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Sets the value of prenom.
+     *
+     * @param mixed $prenom the prenom
+     *
+     * @return self
+     */
+    private function _setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}
